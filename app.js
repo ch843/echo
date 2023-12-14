d6cfad2f50e2b0d819893e076cf07fc974d7805c
reedstew <rcstew120@gmail.com>
2023-12-14 12:47:56 -0700

    change delete get to post in app.js

--- app.js
+++ app.js
@@ -139,7 +139,7 @@ app.post('/addEntry', async (req, res) => {
   });
 });
 
-app.get("/delete/:entryid", async(req, res)=> {
+app.post("/delete/:entryid", async(req, res)=> {
   try {
     await knex("journal").where({ entryid: req.params.entryid }).del();
     res.redirect("/history");
